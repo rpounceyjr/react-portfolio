7e27f6a116a771417624bbb51cc163e693621217
rpounceyjr <rpounceyjr@gmail.com>
2020-06-30 10:32:51 -0400

    added web and github icons as clickable links.  still working on linking project photos to portfolio.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import "./style.css"
 
 function Footer() {
     return(
-        <footer class="navbar navbar-dark bg-dark fixed-bottom justify-content-center">
+        <footer className="navbar navbar-dark bg-dark fixed-bottom justify-content-center">
         Roger Pouncey
         <a href= "https://github.com/rpounceyjr">GitHub</a>
     </footer>
--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./style.css"
 
+
 function PortfolioCard({name, description, gitHub, url, image}) {
     return (
     <div className="col-lg-6 col-xs-12">
                 <div className="card mx-auto">
-                    <img src={image} className="card-img-top" alt={name} />
+                    <img src={process.env.PUBLIC_URL + image} className="card-img-top" alt={name} />
                     <div className="card-body text-center">
                         <h5 className="card-title text-center">{name}</h5>
                         <hr />
                         <p className="card-text">{description}</p>
                         <hr />
-                        {url && <a href={url} className="btn btn-secondary">Click
-                            Here</a>}
-                        <a href={gitHub} className="btn btn-secondary">GitHub</a>
+                        {url && <a href={url} className="btn btn-secondary"><img src= {process.env.PUBLIC_URL + '/images/web-logo.png'} alt="Application URL" width="32px" height="32px" /></a>}
+                        <a href={gitHub} className="btn btn-secondary"><img src= {process.env.PUBLIC_URL + '/images/GitHub-Mark-32px.png'} alt="GitHub" width="32px" height="32px" /></a>
                     </div>
                 </div>
             </div>
