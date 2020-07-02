d884d840fedcda9aa6d526ea6213a15d925d81f5
rpounceyjr <rpounceyjr@gmail.com>
2020-07-02 15:08:16 -0400

    more styling, fixed a container issue, added a media query.

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -8,14 +8,20 @@ function Bio() {
                     <p className="bio-text">Roger Pouncey is a web developer based in Richmond, Virginia.</p>
                 </div>
                 <div className="row bio-line-2">
-                    <p className="bio-text">A background in jazz performance allows him to work well with others and adapt to changing circumstances.</p>
+                    <p className="bio-text">A background in jazz performance allows him to work well with others,</p>
+                </div>
+                <div className="row bio-line-9">
+                    <p className="bio-text">seek creative solutions,</p>
+                </div>
+                <div className="row bio-line-8">
+                    <p className="bio-text">and adapt to changing circumstances.</p>
                 </div>
                 <div className="row bio-line-3">
                     <p className="bio-text">Roger is adept at</p>
                 </div>
                 <div className="row bio-line-4">
-                <p className="bio-text">HTML, CSS, JavaScript, Bootstrap, jQuery, MySQL, and others, </p>
-            </div>
+                    <p className="bio-text">HTML, CSS, JavaScript, Bootstrap, jQuery, MySQL, and other technologies, </p>
+                </div>
                 <div className="row bio-line-5">
                     <p className="bio-text">but he is especially interested in the</p>
                 </div>
